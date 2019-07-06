f4cf53f0a83f15257773c35db82e8b57733fc4bc
NorJull <narendavid10@hotmail.com>
2019-07-05 22:02:00 -0500

    SE crea endpoint para descargar RIPS AP

--- src/main/java/com/vamosaprogramar/umedicalapi/controller/ControladorRIPS.java
+++ src/main/java/com/vamosaprogramar/umedicalapi/controller/ControladorRIPS.java
@@ -4,6 +4,10 @@ import java.io.File;
 import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,9 +23,13 @@ public class ControladorRIPS {
 	private ServicioRIPS servicioRIPS;
 	
 	@GetMapping("/AP/{contratoId}/{factura}/{fechaInicial}/{fechaFinal}")
-	public File obtenerAP(@PathVariable Integer contratoId,@PathVariable String factura,@PathVariable String fechaInicial,@PathVariable String fechaFinal) {
+	public ResponseEntity<Resource> obtenerAP(@PathVariable Integer contratoId,@PathVariable String factura,@PathVariable String fechaInicial,@PathVariable String fechaFinal) {
 		try {
-			return servicioRIPS.obtenerAP(contratoId,factura,fechaInicial, fechaFinal);
+			File file = servicioRIPS.obtenerAP(contratoId,factura,fechaInicial, fechaFinal);
+			Resource fileSystemResource = new FileSystemResource(file);
+			 return ResponseEntity.ok()
+	                    .contentType(MediaType.MULTIPART_FORM_DATA)
+	                    .body(fileSystemResource);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
